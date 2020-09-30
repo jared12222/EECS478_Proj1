29fe44a160c728acda9e48439512033edc522fdd
SOG XRAY <jared12222@gmail.com>
2020-09-30 00:42:20 -0400

    Buggy apply only tried single boolean function yet, unique table isn't working as expected

--- eecs478p1/project1.cpp
+++ eecs478p1/project1.cpp
@@ -36,40 +36,66 @@ bdd_ptr apply(bdd_ptr bdd1, bdd_ptr bdd2, operation &op)
 {
   // get reference to tables
   bdd_tables& tables = bdd_tables::getInstance();
-  
+
   // ... your code goes here
   // change the return value when you're finished
+
+
+
   bdd_ptr node,neg_n,pos_n;
   string str_op = bdd1->var + op.get_operation() + bdd2->var;
-  // cout << "computed node " << str_op << endl;
+  
   if( tables.find_in_computed_table(str_op,bdd1,bdd2)!= 0 )
     return tables.find_in_computed_table(str_op,bdd1,bdd2);
-  node = op(bdd1,bdd2);
-  if( node != 0 )
+  
+  if( bdd1->is_terminal() && bdd2->is_terminal() ) // Could be computed
   {
+    // cout << "Node could be computed!" << endl;
+    node = op(bdd1,bdd2);
     tables.insert_computed_table(str_op,bdd1,bdd2,node);
+    // cout << "Result: " << node << endl;
     return node;
   }
-  else if ( bdd1->is_terminal() )
+  else if( bdd1->is_terminal() == 1 )
   {
+    // cout << "bdd1 is terminal" << endl;
     neg_n = apply(bdd1,bdd2->neg_cf,op.get_operation());
     pos_n = apply(bdd1,bdd2->pos_cf,op.get_operation());
   }
-  else if ( bdd2->is_terminal() )
+  else if ( bdd2->is_terminal() == 1 )
   {
+    // cout << "bdd2 is terminal" << endl;
     neg_n = apply(bdd1->neg_cf,bdd2,op.get_operation());
     pos_n = apply(bdd1->pos_cf,bdd2,op.get_operation());
   }
   else
   {
+    // cout << "No terminal" << endl;
+    if( bdd1->var != bdd2->var )
+    {
+      neg_n = apply(bdd1->neg_cf,bdd2,op.get_operation());
+      pos_n = apply(bdd1->pos_cf,bdd2,op.get_operation());
+      node = tables.create_and_add_to_unique_table(bdd1->var,neg_n,pos_n);
+      return node;
+    }
     neg_n = apply(bdd1->neg_cf,bdd2->neg_cf,op.get_operation());
     pos_n = apply(bdd1->pos_cf,bdd2->pos_cf,op.get_operation());
   }
-
-  // fill in childs into node
-  node->neg_cf = neg_n;
-  node->pos_cf = pos_n;
+    
   
+  // cout << "childs should be computed" << endl;
+  
+  // fill in childs into node
+  char var;
+  if(bdd1->is_terminal())
+    var = bdd2->var;
+  else if(bdd2->is_terminal())
+    var = bdd1->var;
+  node = tables.create_and_add_to_unique_table(var,neg_n,pos_n);
+  // cout << "paran " << node << endl;
+  // cout << "neg_n " << neg_n << endl;
+  // cout << "pos_n " << pos_n << endl;
+  // tables.print_computed_table(); 
   return node;
 }
 
